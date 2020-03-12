9ad8364bb51f5c7cd0b73d83a53d9f6931fbfbed
VerstraeteBert <bertverstraete22@gmail.com>
2020-03-12 21:18:43 +0100

    probeerselv2

--- threading/parallel-merge.c
+++ threading/parallel-merge.c
@@ -1,22 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 #include <pthread.h>
 #include <time.h>
 
-const int SIZE = 10000000;
+const int SIZE = 10;
 const int THREAD_POOL = 4;
+//pthread_mutex_t lock;
 
 void fill_backwards(int [], size_t);
 void* merge_sort_new_thread(void* v_m_data);
 void merge_sort_parallel(int [], size_t);
-void merge_sort_parallel_helper(int arr[], size_t n, int* thread_pool);
-void merge(int [], size_t);
-void merge_sort_seq(int [], size_t);
+void merge_sort_parallel_helper(int arr[], int start, int end, int* thread_pool);
+void merge(int [], int start, int mid, int end);
+void merge_sort_seq(int[], int n);
+void merge_sort_seq_helper(int [], int start, int end);
 
 struct merge_data {
     int * arr;
-    size_t n;
+    int start;
+    int end;
     int* thread_pool;
 } merge_data;
 
@@ -48,62 +50,68 @@ void fill_backwards(int arr [], size_t n) {
 }
 
 void merge_sort_parallel(int arr [], size_t n) {
-    int* thread_pool = malloc(sizeof(int));
-    *thread_pool = THREAD_POOL;
-    merge_sort_parallel_helper(arr, n, thread_pool);
-    free(thread_pool);
+    int thread_pool = THREAD_POOL;
+ //   pthread_mutex_init(&lock, NULL);
+    merge_sort_parallel_helper(arr, 0, (int) n - 1, &thread_pool);
+ //   pthread_mutex_destroy(&lock);
 }
 
-void merge_sort_parallel_helper(int arr[], size_t n, int* thread_pool) {
-    if (n > 1) {
-        if (*thread_pool >= 1) {
-            // spawned thread will handle first half, current thread will handle second half
+void merge_sort_parallel_helper(int arr[], int start, int end, int* thread_pool) {
+    if (start < end) {
+        int mid = (start + end) / 2;
+        //pthread_mutex_lock(&lock);
+        if (*thread_pool >= 2) {
             (*thread_pool) -= 1;
-            struct merge_data* m_data = (struct merge_data*) malloc(sizeof(merge_data));
-            m_data->thread_pool = thread_pool;
-            m_data->n = n/2;
-            m_data->arr = arr;
+         //   pthread_mutex_unlock(&lock);
+         // spawned thread will handle first half, current thread will handle second half
+            struct merge_data m_data = {
+                    .arr = arr,
+                    .start = start,
+                    .end = mid,
+                    .thread_pool = thread_pool
+            };
             pthread_t merge_thr;
-            pthread_create(&merge_thr, NULL, merge_sort_new_thread, (void*) m_data);
+            pthread_create(&merge_thr, NULL, merge_sort_new_thread, (void*) &m_data);
 
-            merge_sort_parallel_helper(arr + n/2, n-n/2, thread_pool);
+            merge_sort_parallel_helper(arr, mid + 1, end, thread_pool);
 
             pthread_join(merge_thr, NULL);
-            free(m_data);
         } else {
-            merge_sort_parallel_helper(arr, n/2, thread_pool);
-            merge_sort_parallel_helper(arr + n/2, n - n/2, thread_pool);
+          //  pthread_mutex_unlock(&lock);
+            merge_sort_parallel_helper(arr, start, mid, thread_pool);
+            merge_sort_parallel_helper(arr, mid + 1, end, thread_pool);
         }
-        merge(arr, n);
+        merge(arr, start, mid, end);
     }
 }
 
 void* merge_sort_new_thread(void* v_m_data) {
     struct merge_data* m_data = (struct merge_data*) v_m_data;
-    merge_sort_parallel_helper(m_data->arr, m_data->n, m_data->thread_pool);
+    merge_sort_parallel_helper(m_data->arr, m_data->start, m_data->end, m_data->thread_pool);
     return NULL;
 }
 
-void merge_sort_seq(int arr [], size_t n) {
-    if (n > 1) {
-        merge_sort_seq(arr, n/2);
-        merge_sort_seq(arr + n/2, n - n/2);
-        merge(arr, n);
+void merge_sort_seq(int arr[], int n) {
+    merge_sort_seq_helper(arr, 0, n - 1);
+}
+
+void merge_sort_seq_helper(int arr [], int start, int end) {
+    if (start < end) {
+        int mid = (start + end) / 2;
+        merge_sort_seq_helper(arr, start, mid);
+        merge_sort_seq_helper(arr, mid + 1, end);
+        merge(arr, start, mid, end);
     }
 }
 
-void merge (int arr [], size_t n) {
-    size_t l = 0;
-    size_t r = (n/2);
-    size_t end = n - 1;
-    size_t mid = n/2 - 1;
-    size_t curr = 0;
+void merge (int arr [], int start, int mid, int end) {
+    int l = start; int r = mid + 1; int curr = 0;
 
     if (arr[mid] <= arr[r]) {
         return;
     }
 
-    int * temp = malloc(n * sizeof(int));
+    int * temp = malloc((end - start + 1)  * sizeof(int));
 
     while (l <= mid && r <= end) {
         if (arr[l] <= arr[r]) {
@@ -124,8 +132,7 @@ void merge (int arr [], size_t n) {
 
     int i;
     for (i = 0; i < curr; i++) {
-        *arr = temp[i];
-        arr++;
+        arr[start++] = temp[i];
     }
     free(temp);
 }
